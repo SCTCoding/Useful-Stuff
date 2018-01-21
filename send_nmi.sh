e4fe512074ff39984aea2877c0e0814483fa45b1
Simon <SCTCoding@users.noreply.github.com>
2018-01-20 23:16:17 -0500

    Create send_nmi.sh

---
+++ send_nmi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+
+/Applications/VMware\ Fusion.app/Contents/Library/vmrun suspend $DIR/*.vmx
+perl -i -pe 's/(?<=pendingNMI\x00{4})\x00/\x01/' *.vmss
+/Applications/VMware\ Fusion.app/Contents/Library/vmrun start $DIR/*.vmx
+
+# Credit
+# https://cloudtone.wordpress.com/2012/12/13/vmware-fusion-can-you-diag-it-nmi/
