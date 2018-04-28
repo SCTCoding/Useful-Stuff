d3bc737b66217598d4de1893fa39b23babb9c8ef
Simon <SCTCoding@users.noreply.github.com>
2018-04-28 19:21:28 -0400

    Update Local_Items_Management.sh

--- Local_Items_Management.sh
+++ Local_Items_Management.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# This script is for basic local items keychain management for debug/utility drives.
+# This is useful for any time you have a drive that is connected to many computers,
+# and you want to avoid the issue of run-away local items keychains.
+# This script is best run as a launch agent.
+
 # Navigate to the user keychain folder.
 cd /Users/debug/Library/Keychains
 
@@ -11,5 +16,13 @@ for i in ${lnames[@]}
 do
 	rm -R $i
 done
+
+# Stop trustd and secd.
+launchctl stop com.apple.secd
+launchctl stop com.apple.trustd.agent
+
+# Start trustd and secd
+launchctl start com.apple.secd
+launchctl start com.apple.trustd.agent
 
 exit
