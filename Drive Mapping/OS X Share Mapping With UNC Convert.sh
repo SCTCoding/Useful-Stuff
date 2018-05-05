a3d0589343aae10f043edd6077377d6a5aa39b0b
Simon <SCTCoding@users.noreply.github.com>
2018-05-04 20:22:47 -0400

    Update OS X Share Mapping With UNC Convert.sh

--- Drive Mapping/OS X Share Mapping With UNC Convert.sh
+++ Drive Mapping/OS X Share Mapping With UNC Convert.sh
@@ -9,32 +9,28 @@ prefix=`osascript -e 'set choice to (choose from list {"Screen Share", "File Sha
 # General Logic
 if [[ $prefix == "false" ]]
 then
-  # If prefix is an error then exit the script. In other words if the user cancels don't do anything.
+  	# If prefix is an error then exit the script. In other words if the user cancels don't do anything.
 	exit
 else
   # Otherwise lets do stuff logic follows.
 	if [[ $prefix == "Screen Share" ]]
 	then
-    # If screen share then determine the path.
+		# If screen share then determine the path.
 		path=`osascript -e 'set T to text returned of (display dialog "What share would you like to add? (Should start with vnc://)" buttons {"Cancel", "OK"} default button "OK" default answer "vnc://")'`
 		# Converts UNC to OS X useful path. Relies on proper host specification. Either spelled out or with search domains.
-    unc_slash=$(echo -n $path | sed -e 's/\\/\//g' | cut -c2-100000)
-    unc_conv=$(echo -n "vnc://$unc_slash"
-    # Add path to shared file list.
-    /usr/bin/sfltool add-item -n "$path" com.apple.LSSharedFileList.FavoriteServers "$path"
+		unc_slash=$(echo -n $path | sed -e 's/\\/\//g' | cut -c2-100000)
+		unc_conv=$(echo -n "vnc://$unc_slash")
+    		# Add path to shared file list.
+    		/usr/bin/sfltool add-item -n "$path" com.apple.LSSharedFileList.FavoriteServers "$path"
 		# Open path
-    open $path
+		open $path
 	else
-    # Same basic flow as above but for SMB.
+   		# Same basic flow as above but for SMB.
 		path=`osascript -e 'set T to text returned of (display dialog "What share would you like to add? (Should start with smb://)" buttons {"Cancel", "OK"} default button "OK" default answer "smb://")'`
-    unc_slash=$(echo -n $path | sed -e 's/\\/\//g' | cut -c2-100000)
-    unc_conv=$(echo -n "smb://$unc_slash"
+		unc_slash=$(echo -n $path | sed -e 's/\\/\//g' | cut -c2-100000)
+    		unc_conv=$(echo -n "smb://$unc_slash")
 		/usr/bin/sfltool add-item -n "$path" com.apple.LSSharedFileList.FavoriteServers "$path"
 		open $path
 	fi
 fi
 exit
-
-
-
-temp=$(echo -n "\\test\test\test" | sed -e 's/\\/\//g' | cut -c2-1000); echo "afp://"$temp
