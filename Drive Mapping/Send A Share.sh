c0b3e5ecd962ce8683f02caf28d1b61b20f3bc46
Simon <SCTCoding@users.noreply.github.com>
2018-05-04 20:36:52 -0400

    Update Send A Share.sh

--- Drive Mapping/Send A Share.sh
+++ Drive Mapping/Send A Share.sh
@@ -2,12 +2,20 @@
 
 # Purpose of this script is to provide a simple double-click share add method.
 
+# Define what paths we are going to add
 declare -a paths=(
   # Paths in quotes
 )
 
 for i in ${paths[@]}
 do
-  /usr/bin/sfltool add-item -n "$i" com.apple.LSSharedFileList.FavoriteServers "$i"
-	open $path
+	# UNC Converter
+	unc_slash=$(echo -n $i | sed -e 's/\\/\//g' | cut -c2-100000)
+	unc_conv=$(echo -n "vnc://$unc_slash")
+	
+	# Add Path
+	/usr/bin/sfltool add-item -n "$unc_conv" com.apple.LSSharedFileList.FavoriteServers "$unc_conv"
+	
+	# Open each
+	open $unc_conv
 done
