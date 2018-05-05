c0bd522b0214f6f0898d935d6d84277372fc2ee8
Simon <SCTCoding@users.noreply.github.com>
2018-05-04 20:41:00 -0400

    Update Send A Share.sh

--- Drive Mapping/Send A Share.sh
+++ Drive Mapping/Send A Share.sh
@@ -11,7 +11,7 @@ for i in ${paths[@]}
 do
 	# UNC Converter
 	unc_slash=$(echo -n $i | sed -e 's/\\/\//g' | cut -c2-100000)
-	unc_conv=$(echo -n "vnc://$unc_slash")
+	unc_conv=$(echo -n "smb://$unc_slash")
 	
 	# Add Path
 	/usr/bin/sfltool add-item -n "$unc_conv" com.apple.LSSharedFileList.FavoriteServers "$unc_conv"
