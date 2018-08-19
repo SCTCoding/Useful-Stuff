2825f0a42ec6ff30d74de57cae410eb31d4cf5eb
Simon <SCTCoding@users.noreply.github.com>
2018-08-18 21:38:35 -0400

    Add files via upload

--- Admin Installer Script.sh
+++ Admin Installer Script.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
-# Obatin username and password
+# Obatin username, gid, and password
 username=$(osascript -e 'set inputuser to text returned of (display dialog "What username would you like?" default answer "")')
 
 password=$(osascript -e 'set inputpw to text returned of (display dialog "What password would you like?" default answer "" with hidden answer)')
+
+gidinput=$(osascript -e 'set inputgid to text returned of (display dialog "What GID would you like?" default answer "510" with hidden answer)')
 
 # Create the user
 sudo dscl . -create /Users/"${username}"
 sudo dscl . -create /Users/"${username}" UserShell /bin/bash
 sudo dscl . -create /Users/"${username}" RealName "${username}" 
-sudo dscl . -create /Users/"${username}" UniqueID "510"
+sudo dscl . -create /Users/"${username}" UniqueID "${gidinput}"
 sudo dscl . -create /Users/"${username}" PrimaryGroupID 20
 sudo dscl . -create /Users/"${username}" NFSHomeDirectory /Users/"${username}"
 sudo dscl . -passwd /Users/"${username}" "${password}"
